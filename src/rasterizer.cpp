2a5410e7eee59ace5301f52b29215cc88b42e4a5
sme777 <samson.petrosyan8@gmail.com>
2022-02-17 13:00:01 -0800

    finish task 1 and task 2

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -22,8 +22,10 @@ namespace CGL {
     // NOTE: You are not required to implement proper supersampling for points and lines
     // It is sufficient to use the same color for all supersamples of a pixel for points and lines (not triangles)
 
+    for (int w = 0; w < sample_rate; w++) {
+        sample_buffer[sample_rate * (y * width + x) + w] = c;
+    }
 
-    sample_buffer[y * width + x] = c;
   }
 
   // Rasterize a point: simple example to help you start familiarizing
@@ -71,41 +73,44 @@ namespace CGL {
     float x2, float y2,
     Color color) {
     // TODO: Task 1: Implement basic triangle rasterization here, no supersampling
-        float tri = area(x0, y0, x1, y1, x2, y2);
-        for (int i = 0; i < this->width; i++) {
-            for (int j = 0; j < this->height; j++) {
-                float x = (float) i + 0.5;
-                float y = (float) j + 0.5;
+    // TODO: Task 2: Update to implement super-sampled rasterization
+    auto samples = (float) (sqrt(sample_rate));
+    float x;
+    float y;
+    for (int i = 0; i < this->width; i++) {
+        for (int j = 0; j < this->height; j++) {
+            int count = 0;
+            for (int w = 0; w < samples; w++) {
+                for (int z = 0; z  < samples; z++) {
 
-                /** Approach 1
-                 * if (inside(tri, x0, y0, x1, y1, x2, y2, x, y)) {
-                    fill_pixel(i, j, color);
-                   }
-                 */
-                /** Approach 2
-                 */
-                 if (insideTri(x0, y0, x1, y1, x2, y2, x, y)) {
-                     fill_pixel(i, j, color);
-                 }
+                    x = (2*((float) i) + ((float) w)/samples + ((float) w + 1)/samples)/2;
+                    y = (2*((float) j) + ((float) z)/samples + ((float) z + 1)/samples)/2;
+
+                    if (insideTri(x0, y0, x1, y1, x2, y2, x, y)) {
+                        sample_buffer[sample_rate * (j * width + i) + count] = color;
+                    }
+                    count++;
+                }
             }
         }
-
-
-    /** Approach 2
-     *
-     *
-     */
-
-    // TODO: Task 2: Update to implement super-sampled rasterization
-
-
-
+    }
   }
 
   bool RasterizerImp::insideTri(float x0, float y0,
                                 float x1, float y1,
                                 float x2, float y2,
                                 float i, float j) {
+    float first_plane = -(i - x0)*(y0-y2) + (j - y0)*(x0-x2);
+    float second_plane = -(i - x1)*(y1-y0) + (j - y1)*(x1-x0);
+    float third_plane = -(i - x2)*(y2-y1) + (j - y2)*(x2-x1);
+
+    float fourth_plane = -(i - x2)*(y1-y2) + (j - y2)*(x1-x2);
+    float fifth_plane = -(i - x1)*(y0-y1) + (j - y1)*(x0-x1);
+    float sixth_plane = -(i - x0)*(y2-y0) + (j - y0)*(x2-x0);
+
+
+    return (first_plane >= 0 && second_plane >= 0 && third_plane >= 0) ||
+            (fourth_plane >=0 && fifth_plane >= 0 && sixth_plane >= 0);
 
   }
 
@@ -161,8 +166,7 @@ namespace CGL {
 
     this->sample_rate = rate;
 
-
-    this->sample_buffer.resize(width * height, Color::White);
+    this->sample_buffer.resize(width * height * rate, Color::White);
   }
 
 
@@ -175,8 +179,7 @@ namespace CGL {
     this->height = height;
     this->rgb_framebuffer_target = rgb_framebuffer;
 
-
-    this->sample_buffer.resize(width * height, Color::White);
+    this->sample_buffer.resize(width * height * sample_rate, Color::White);
   }
 
 
@@ -193,15 +196,27 @@ namespace CGL {
   //
   void RasterizerImp::resolve_to_framebuffer() {
     // TODO: Task 2: You will likely want to update this function for supersampling support
-
-
     for (int x = 0; x < width; ++x) {
       for (int y = 0; y < height; ++y) {
-        Color col = sample_buffer[y * width + x];
+        Color col = sample_buffer[sample_rate * (y * width) + x];
+        float avg_r = 0;
+        float avg_g = 0;
+        float avg_b = 0;
 
-        for (int k = 0; k < 3; ++k) {
-          this->rgb_framebuffer_target[3 * (y * width + x) + k] = (&col.r)[k] * 255;
+        for (int w = 0; w < sample_rate; w++) {
+            avg_r += sample_buffer[(y * width + x) * sample_rate + w].r * 255;
+            avg_g += sample_buffer[(y * width + x) * sample_rate + w].g * 255;
+            avg_b += sample_buffer[(y * width + x) * sample_rate + w].b * 255;
         }
+
+        avg_r /= (float) sample_rate;
+        avg_g /= (float) sample_rate;
+        avg_b /= (float) sample_rate;
+
+        this->rgb_framebuffer_target[3 * (y * width + x)] = (unsigned char) avg_r;
+        this->rgb_framebuffer_target[3 * (y * width + x) + 1] = (unsigned char) avg_g;
+        this->rgb_framebuffer_target[3 * (y * width + x) + 2] = (unsigned char) avg_b;
+
       }
     }
 
